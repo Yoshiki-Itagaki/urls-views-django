54e99a34cf896cb002b18d3016422a3142f908b6
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-14 22:33:02 +0900

    Added more dynamic view logic

--- challenges/views.py
+++ challenges/views.py
@@ -1,29 +1,31 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 
+challenges = {
+    "january":"Eat no meat for the entire month!" ,
+    "february":"Walk for at least 20 minutes every day!",
+    "march":"Learn Django for at least 20 minutes every day!",
+    "april":"Eat no meat for the entire month!" ,
+    "may":"Walk for at least 20 minutes every day!",
+    "june":"Learn Django for at least 20 minutes every day!",
+    "july":"Eat no meat for the entire month!" ,
+    "august":"Walk for at least 20 minutes every day!",
+    "september":"Learn Django for at least 20 minutes every day!",
+    "october":"Eat no meat for the entire month!" ,
+    "november":"Walk for at least 20 minutes every day!",
+    "december":"Learn Django for at least 20 minutes every day!",
+}
+
 # Create your views here.
-
-def january(request):
-    print(request)
-    return HttpResponse("Eat no meat for the entire month!")
-
-def february(request):
-    return HttpResponse("Walk for at least 20 minutes every day!")
-
-def march(request):
-    return HttpResponse("Learn Django for at least 30 miniutes per day")
 
 def get_month_by_number(request, month):
     return HttpResponse(month)
 
 def get_month(request, month):
-    if month == "january":
-        msg = "Eat no meat for the entire month!" 
-    elif month == "february":
-        msg = "Walk for at least 20 minutes every day!"
-    elif month == "march":
-        msg = "Learn Django for at least 20 minutes every day!"
-    else:
-        return HttpResponseNotFound("This month is not supported!")        
+    try:
+        msg = challenges[month]
+        return HttpResponse(msg)
+    except:
+        return HttpResponse("This month is not supported")
         
-    return HttpResponse(msg)
+    
