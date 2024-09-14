d7c643ba2008fbf5f93748cb553bb381434604b9
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-15 02:49:09 +0900

    Practiced URLs, views & dynamic view logic

--- challenges/urls.py
+++ challenges/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.index),
     path("<int:month>", views.get_month_by_number),
-    path("<str:month>", views.get_month, name="monthly-challenge")
+    path("<str:month>", views.get_month, name="month-challenge")
 ]
--- challenges/views.py
+++ challenges/views.py
@@ -19,6 +19,18 @@ challenges = {
 
 # Create your views here.
 
+def index(request):
+    list_items = ""
+    months = list(challenges.keys())
+    
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse("month-challenge", args=[month])
+        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    
+    responseData = f"<ul>{list_items}</ul>"
+    return HttpResponse(responseData)
+
 def get_month_by_number(request, month):
     months = list(challenges.keys())
         
@@ -26,14 +38,15 @@ def get_month_by_number(request, month):
         return HttpResponse("Invalid month")
             
     redirect_month = months[month - 1]
-    redirect_path = reverse("monthly-challenge", args=[redirect_month])
+    redirect_path = reverse("month-challenge", args=[redirect_month])
     return HttpResponseRedirect(redirect_path)
 
 def get_month(request, month):
     try:
         msg = challenges[month]
-        return HttpResponse(msg)
+        response_data = f"<h1>{msg}</h1>"
+        return HttpResponse(response_data)
     except:
-        return HttpResponse("This month is not supported")
+        return HttpResponse("<h1>This month is not supported!</h1>")
         
     
