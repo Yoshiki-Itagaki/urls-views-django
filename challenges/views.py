c5c4aadbf98a4d4fa994b69464288ddaef21506c
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-14 22:47:26 +0900

    Coded up redirected logic

--- challenges/views.py
+++ challenges/views.py
@@ -1,25 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 
 challenges = {
-    "january":"Eat no meat for the entire month!" ,
-    "february":"Walk for at least 20 minutes every day!",
-    "march":"Learn Django for at least 20 minutes every day!",
-    "april":"Eat no meat for the entire month!" ,
-    "may":"Walk for at least 20 minutes every day!",
-    "june":"Learn Django for at least 20 minutes every day!",
-    "july":"Eat no meat for the entire month!" ,
-    "august":"Walk for at least 20 minutes every day!",
-    "september":"Learn Django for at least 20 minutes every day!",
-    "october":"Eat no meat for the entire month!" ,
-    "november":"Walk for at least 20 minutes every day!",
-    "december":"Learn Django for at least 20 minutes every day!",
+    "january": "january".capitalize() + ": Eat no meat for the entire month!" ,
+    "february": "february".capitalize() + ": Walk for at least 20 minutes every day!",
+    "march": "march".capitalize() + ": Learn Django for at least 20 minutes every day!",
+    "april": "april".capitalize() + ": Eat no meat for the entire month!" ,
+    "may": "may".capitalize() + ": Walk for at least 20 minutes every day!",
+    "june": "june".capitalize() + ": Learn Django for at least 20 minutes every day!",
+    "july": "july".capitalize() + ": Eat no meat for the entire month!" ,
+    "august": "august".capitalize() + ": Walk for at least 20 minutes every day!",
+    "september": "september".capitalize() + ": Learn Django for at least 20 minutes every day!",
+    "october": "october".capitalize() + ": Eat no meat for the entire month!" ,
+    "november": "november".capitalize() + ": Walk for at least 20 minutes every day!",
+    "december": "december".capitalize() + ": Learn Django for at least 20 minutes every day!",
 }
 
 # Create your views here.
 
 def get_month_by_number(request, month):
-    return HttpResponse(month)
+    months = list(challenges.keys())
+    
+    print(months)
+    
+    if month == 0 or month > len(months):
+        return HttpResponse("Invalid month")
+            
+    redirect_month = months[month - 1]
+    return HttpResponseRedirect("/challenges/" + redirect_month)
 
 def get_month(request, month):
     try:
